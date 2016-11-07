dd34c46a86a1f304d44d51288509aa27780653f6
Ikechukwu Francis Uwazurike <uwazurike@gmail.com>
2016-11-07 06:40:45 -0800

    To Do List App and Bubble Sort

---
+++ ToDoList/ToDoList/Controllers/RecordsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList;
+
+namespace ToDoList.Controllers
+{
+    public class RecordsController : Controller
+    {
+        private Taska2Entities db = new Taska2Entities();
+
+        // GET: Records
+        public ActionResult Index()
+        {
+            var records = db.Records.Include(r => r.Chore);
+            return View(records.ToList());
+        }
+
+        // GET: Records/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // GET: Records/Create
+        public ActionResult Create()
+        {
+            ViewBag.TaskID = new SelectList(db.Chores, "TaskID", "TaskTitle");
+            return View();
+        }
+
+        // POST: Records/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserName,TaskID")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Records.Add(record);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TaskID = new SelectList(db.Chores, "TaskID", "TaskTitle", record.TaskID);
+            return View(record);
+        }
+
+        // GET: Records/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TaskID = new SelectList(db.Chores, "TaskID", "TaskTitle", record.TaskID);
+            return View(record);
+        }
+
+        // POST: Records/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserName,TaskID")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(record).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TaskID = new SelectList(db.Chores, "TaskID", "TaskTitle", record.TaskID);
+            return View(record);
+        }
+
+        // GET: Records/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // POST: Records/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Record record = db.Records.Find(id);
+            db.Records.Remove(record);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Models/ToDoList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList
+{
+
+    [MetadataType(typeof(ChoreMetaData))]
+    public partial class Chore
+    {
+    }
+
+    public class ChoreMetaData
+    {
+        [DisplayName("Task")]
+        public int TaskID { get; set; }
+
+        [DisplayName("Task")]
+        [Required(ErrorMessage = "Please enter a Task Title")]
+        [MaxLength(150, ErrorMessage = "Task Title can't be more than 150 characters")]
+        public string TaskTitle { get; set; }
+
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "A description is required")]
+        [MaxLength(150, ErrorMessage = "Keep Task Descriptions under 150 characters")]
+        public string TaskDescription { get; set; }
+
+        [DisplayName("Category")]
+        public Nullable<int> TaskCategory { get; set; }
+
+        [DisplayName("Created")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "A Creation date is required")]
+        public System.DateTime DateCreated { get; set; }
+
+        [DisplayName("Due")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "A Due date is required")]
+        public System.DateTime DueDate { get; set; }
+
+        [DisplayName("Completed")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DateCompleted { get; set; }
+    }
+
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category
+    {
+    }
+
+    public class CategoryMetaData
+    {
+        [DisplayName("Category")]
+        public int CategoryID { get; set; }
+
+        [DisplayName("Category")]
+        [Required(ErrorMessage = "Please enter a Category name")]
+        [MaxLength(150, ErrorMessage = "Category name can't be more than 150 characters")]
+        public string CategoryName { get; set; }
+    }
+
+    [MetadataType(typeof(RecordMetaData))]
+    public partial class Record
+    {
+    }
+
+    public class RecordMetaData
+    {
+        [DisplayName("User")]
+        [Required(ErrorMessage = "Please enter a UserName")]
+        [MaxLength(150, ErrorMessage = "UserName can't be more than 150 characters")]
+        public string UserName { get; set; }
+
+        [DisplayName("Task")]
+        public int TaskID { get; set; }
+    }
+}
